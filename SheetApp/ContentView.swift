a9f5baf8bd34ee4d8aa10218cdb90024905eac8f
Ruxiu0409 <enokgood2@gmail.com>
2024-09-02 23:22:05 +0800

    Initial Commit

---
+++ SheetApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SheetApp
+//
+//  Created by 蔡承曄 on 2024/9/2.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SheetApp/SheetAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SheetAppApp.swift
+//  SheetApp
+//
+//  Created by 蔡承曄 on 2024/9/2.
+//
+
+import SwiftUI
+
+@main
+struct SheetAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
